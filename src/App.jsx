629ced6e0edd28384818914986778a0c456ed8db
Awais Akram <awaisakram182@gmail.com>
2025-04-12 13:29:59 +0500

    Prepare for deployment via gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import Solution from './Pages/Solution';
 import Contact from './Pages/Contact';
 import Updates from './Pages/Updates';
 import { Routes, Route } from "react-router-dom";
-import Hero from './Pages/Hero';
+import Hero from './Pages/Hero'
+
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Hero />} />
-      <Route path="/About" element={<About />} />
-      <Route path="/Solution" element={<Solution />} />
-      <Route path="/Updates" element={<Updates />} />
-      <Route path="/Contact" element={<Contact />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Solution" element={<Solution />} />
+        <Route path="/Updates" element={<Updates />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 };
 
