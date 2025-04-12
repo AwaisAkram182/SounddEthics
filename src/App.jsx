976b5517448185fa22a7af2447ef6f3178dc17f7
Awais Akram <awaisakram182@gmail.com>
2025-04-12 13:19:28 +0500

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,18 @@ import About from './Pages/About';
 import Solution from './Pages/Solution';
 import Contact from './Pages/Contact';
 import Updates from './Pages/Updates';
-import {HashRouter as Routes, Route } from "react-router-dom";
-import Hero from './Pages/Hero'
-
+import { Routes, Route } from "react-router-dom";
+import Hero from './Pages/Hero';
 
 const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Solution" element={<Solution />} />
-        <Route path="/Updates" element={<Updates />} />
-        <Route path="/Contact" element={<Contact />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Hero />} />
+      <Route path="/About" element={<About />} />
+      <Route path="/Solution" element={<Solution />} />
+      <Route path="/Updates" element={<Updates />} />
+      <Route path="/Contact" element={<Contact />} />
+    </Routes>
   );
 };
 
